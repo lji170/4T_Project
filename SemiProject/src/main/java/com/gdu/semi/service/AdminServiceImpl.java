1e13d44bbde82320b934dba0161a3fc436da596d
laudable0603 <109578835+laudable0603@users.noreply.github.com>
2022-11-30 17:32:28 +0900

    Ys (#9)
    * [ys]admin_jsp create
    
    * [ys]ListMapper
    
    * [ys] 퇴화
    
    * [ys] controller mapping 수정
    
    * [ys] retireUser구현중
    
    * [ys] 퇴화중
    
    * [ys] 브런치 합병

---
+++ SemiProject/src/main/java/com/gdu/semi/service/AdminServiceImpl.java
@@ -0,0 +1,93 @@
+package com.gdu.semi.service;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gdu.semi.domain.RetireUserDTO;
+import com.gdu.semi.domain.UserDTO;
+import com.gdu.semi.mapper.AdminMapper;
+import com.gdu.semi.util.PageUtil;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+	
+	@Autowired 
+	private AdminMapper adminMapper;
+	
+	@Autowired 
+	private PageUtil pageUtil;
+	
+	@Override
+	public Map<String, Object> getUserList(HttpServletRequest request) {
+		int page = Integer.parseInt(request.getParameter("page"));
+		
+		int userCount = adminMapper.selectUserListCount();
+		
+		pageUtil.setPageUtil(page, 3, userCount);
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("begin", pageUtil.getBegin());
+		map.put("end", pageUtil.getEnd());
+		
+		Map<String, Object> result = new HashMap<>();
+		
+		result.put("userList", adminMapper.selectUserList(map));
+		result.put("pageUtil", pageUtil);
+		
+		return result;
+		
+	}
+	@Override
+	public Map<String, Object> findUser(HttpServletRequest request) {
+		String column = request.getParameter("column");
+		String searchText = request.getParameter("searchText");
+		int page = Integer.parseInt(request.getParameter("page"));
+		
+		Map<String,	Object> userCountMap = new HashMap<>();
+		userCountMap.put("column", column);
+		userCountMap.put("searchText",searchText);
+		
+		int userCount = adminMapper.selectUsersByQueryCount(userCountMap);
+		
+		pageUtil.setPageUtil(page, 3, userCount);
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("begin", pageUtil.getBegin());
+		map.put("end", pageUtil.getEnd());
+		map.put("column", column);
+		map.put("searchText",searchText);
+		
+		Map<String, Object> result = new HashMap<>();
+		result.put("findUserList", adminMapper.selectUsersByQuery(map));
+		result.put("pageUtil", pageUtil);
+		
+		return result;
+		
+	}
+	
+	@Override
+	public Map<String, Object> removeUser(Map<String, Object> userNo) {
+		
+		Map<String,	Object> deleteUser = new HashMap<>();
+		System.out.println(userNo);
+		List<UserDTO> users = adminMapper.selectUserByNo(userNo);
+		users.get(0).getName();
+		List<RetireUserDTO> retireUsersDTO = new ArrayList<>();
+		retireUsersDTO.add(RetireUserDTO);
+		adminMapper.insertUserByNo();
+		deleteUser.put("isRemove", adminMapper.deleteUserByNo(userNo) == 1);
+		
+		
+		return deleteUser;
+		
+	}
+}
