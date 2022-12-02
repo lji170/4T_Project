ad6f154e988973e95c5899343c889472bfee03b3
jje0ngyu <109578927+jje0ngyu@users.noreply.github.com>
2022-12-02 10:47:45 +0900

    Yj (#23)
    * [YJ] test
    
    * [YJ] tomcat
    
    * [YJ] modify gallery_list
    
    * [YJ] gallery
    
    * [YJ] Having written select query
    
    * [YJ] Have to find error of type PRIMITIVE
    
    * [YJ] Have coding likeCount
    
    * [YJ] Haing done comment
    
    * [YJ] modifing summernote
    
    * [YJ] making btn_Like
    
    * [YJ] have to complete LIKE function
    
    * [YJ] creating LIKE function
    
    * [YJ] Like function
    
    * [YJ] making Like function
    
    * [YJ] mmmmmm
    
    * [YJ] 깃 EJ병합
    
    * [YJ] 게시판별 <a> 태그 추가
    
    * [YJ] 깃 합병 전
    
    * [YJ] 로그인 해야 작성/수정/삭제 가능
    
    * [YJ] SecurityUtil 복호화 추가
    
    * [YJ] 갑자기 깃이 안 되는 이유?..
    
    * [YJ] 11장 코드 추가
    
    * [YJ] 이미지 첨부확인  왜 안대
    
    * [YJ] search 수정중

--- SemiProject/src/main/java/com/gdu/semi/service/GalleryServiceImpl.java
+++ SemiProject/src/main/java/com/gdu/semi/service/GalleryServiceImpl.java
@@ -3,6 +3,7 @@ package com.gdu.semi.service;
 import java.io.File;
 import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -11,8 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
@@ -40,14 +39,20 @@ public class GalleryServiceImpl implements GalleryService {
 		this.myFileUtil = myFileUtil;
 	}
 	
+	
+	// 갤러리 목록 나타내기
+	// 목록 개수 : 10, 15, 20
 	@Override
 	public void getGalleryList(HttpServletRequest request, Model model) {
 		
-		// 파라미터
-		Optional<String> opt = Optional.ofNullable(request.getParameter("page"));
-		int page = Integer.parseInt(opt.orElse("1"));
-		Optional<String> rppOpt = Optional.ofNullable(request.getParameter("recordPerPage"));
-		int recordPerPage = Integer.parseInt(rppOpt.orElse("10"));
+		// page 파라미터가 전달되지 않는 경우 page = 1로 처리한다.
+		Optional<String> opt3 = Optional.ofNullable(request.getParameter("page"));
+		int page = Integer.parseInt(opt3.orElse("1"));
+		
+		// recordPerPage는 세션에서 가져오는데 만약 세션에 없으면 10으로 처리한다.
+		HttpSession session = request.getSession();
+		Optional<Object> opt4 = Optional.ofNullable(session.getAttribute("recordPerPage"));
+		int recordPerPage = (int)(opt4.orElse(10));
 		
 		// 전체 블로그 개수
 		int totalRecord = galleryMapper.selectGalleryListCount();
@@ -60,14 +65,65 @@ public class GalleryServiceImpl implements GalleryService {
 		map.put("begin", pageUtil.getBegin());
 		map.put("end", pageUtil.getEnd());
 		
+		// begin~end 목록 가져오기
+		List<GalleryDTO> galleries = galleryMapper.selectGalleryListByMap(map);
+		
 		// 뷰로 전달할 데이터를 model에 저장하기
 		model.addAttribute("totalRecord", totalRecord);
-		model.addAttribute("galleryList", galleryMapper.selectGalleryListByMap(map));
+		model.addAttribute("galleryList", galleries);
 		model.addAttribute("beginNo", totalRecord - (page - 1) * pageUtil.getRecordPerPage());
 		model.addAttribute("paging", pageUtil.getPaging(request.getContextPath() + "/gallery/list"));
 		galleryMapper.selectGalleryList();
 	
 	}
+	// 이미지 첨부된 갤러리 구분하기
+	@Override
+	public Map<String, Object> checkAttachedImage(int galNo) {
+		Map<String, Object> result = new HashMap<>();
+		result.put("isAttached", galleryMapper.selectGallerySummmernote(galNo));
+		return result;
+	}
+	
+	// 검색
+	@Override
+	public void findGalleryList(HttpServletRequest request, Model model) {
+		// recordPerPage 파라미터가 전달되지 않는 경우 10으로 처리한다.
+		Optional<String> opt1 = Optional.ofNullable(request.getParameter("recordPerPage"));
+		int recordPerPage = Integer.parseInt(opt1.orElse("10"));
+		
+		// page 파라미터가 전달되지 않는 경우 1로 처리한다.
+		Optional<String> opt2 = Optional.ofNullable(request.getParameter("page"));
+		int page = Integer.parseInt(opt2.orElse("1"));
+		
+		// 검색 대상
+		String column = request.getParameter("column");
+		
+		// 검색어
+		String query = request.getParameter("query");
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("column", column);
+		map.put("query", query);
+		
+		int totalRecord = galleryMapper.selectFindGalleryListCount(map);
+		
+		pageUtil.setPageUtil(page, recordPerPage, totalRecord);
+		
+		map.put("begin", pageUtil.getBegin());
+		map.put("end", pageUtil.getEnd());
+		
+		List<GalleryDTO> galleryList = galleryMapper.selectFindGalleryList(map);
+		
+		model.addAttribute("galleryList", galleryList);
+		model.addAttribute("beginNo", totalRecord - (page - 1) * pageUtil.getRecordPerPage());
+		
+		String path = null;
+		path = request.getContextPath() + "/gallery/search?column=" + column + "&query=" + query;
+		
+		model.addAttribute("paging", pageUtil.getPaging(path));
+		
+	}
+	
 	
 	@Transactional
 	@Override
@@ -84,7 +140,7 @@ public class GalleryServiceImpl implements GalleryService {
 				.galContent(galContent)
 				.ip(ip)
 				.build();
-		
+		System.out.println(gallery);
 		// DB에 저장
 		int result = galleryMapper.insertGallery(gallery);
 		
@@ -97,6 +153,7 @@ public class GalleryServiceImpl implements GalleryService {
 			if (result > 0) {
 				// 파라미터 summernoteImageNames
 				String[] summernoteImageNames = request.getParameterValues("summernoteImageNames");
+				System.out.println("summernoteImageNames: " + summernoteImageNames);
 				
 				// DB에 SummernoteImage 저장
 				if(summernoteImageNames !=  null) {
@@ -109,12 +166,16 @@ public class GalleryServiceImpl implements GalleryService {
 					}
 				}
 				out.println("alert('갤러리 삽입 성공');");
+				
+				if (!id.equals("admin")) {
+					
 				int point = galleryMapper.updateUserPoint(id);
 				
 				if (point > 0) {
 					out.println("alert('포인트를 10점 적립하였습니다.');");
 				} else {
 					out.println("alert('포인트 적립에 실패했습니다. 게시글 작성이 취소됩니다.');");
+				}
 				}
 				out.println("location.href='"+ request.getContextPath() +"/gallery/list';");
 			} else {
@@ -126,6 +187,7 @@ public class GalleryServiceImpl implements GalleryService {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
 	
 	@Override
@@ -214,11 +276,13 @@ public class GalleryServiceImpl implements GalleryService {
 		String id = request.getParameter("id");
 		int galNo = Integer.parseInt(request.getParameter("galNo"));
 		LikeDTO like = LikeDTO.builder()
-				.id(id)
 				.galNo(galNo)
+				.id(id)
 				.build();
 		
 		int result = galleryMapper.selectLikeUser(like);
+		System.out.println(result);
+		
 		if(result == 0) {
 			galleryMapper.insertLike(like);
 		} else {
